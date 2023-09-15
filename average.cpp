7e5020a3ec00b9df9d4bff374422f8b7038c5676
Jonathan Walker <139590010+JonathanWalkerCS@users.noreply.github.com>
2023-09-14 23:58:00 -0400

    average.cpp

---
+++ average.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+/*
+Date: 9/14/2023 
+Goal: Find the average of three pre-selected numbers.
+Course: Object-oriented programming
+*/
+int num1 = 125, num2 = 28, num3 = -25, average;
+
+average = (num1 + num2 + num3) / 3; 
+cout << "The first number entered is: " << num1 << endl;
+cout << "The second number entered is: " << num2 << endl;
+cout << "The third number entered is: " << num3 << endl;
+cout << "The average of those numbers is: " << average << endl;
+return 0;
+}
