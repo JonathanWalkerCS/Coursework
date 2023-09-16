70b5e8ae1c924c85b8e0e2e61986ee17627d7b93
Jonathan Walker <139590010+JonathanWalkerCS@users.noreply.github.com>
2023-09-15 23:34:46 -0400

    profit.cpp

---
+++ profit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+/*
+Date: 1/20/2023
+Goal: To display a company's annual profit uses the total sales of a company to calculate their profit, 
+then display it.
+Course: Fundamentals of programming
+*/
+  float totalsales = 44000;
+  float profit;
+ 
+  profit = totalsales * 0.23;
+  cout << "The annual profit is $" << profit << endl;
+
+return 0;
+}
